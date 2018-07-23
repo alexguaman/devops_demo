bbe558af15e31d5c9a025894076d7cec060a3ce0
Alex Guaman <alex.guaman86@gmail.com>
2018-07-22 22:26:52 -0500

    Deployment final

--- deployments/prod.sh
+++ deployments/prod.sh
@@ -1,4 +1,11 @@
-wget -O packer.zip https://releases.hashicorp.com/packer/1.2.5/packer_1.2.5_linux_amd64.zip
-unzip packer.zip
-./packer validate deployments/template.json
-./packer build deployments/template.json
+wget -O /tmp/packer.zip https://releases.hashicorp.com/packer/1.2.5/packer_1.2.5_linux_amd64.zip
+wget -O /tmp/terraform.zip https://releases.hashicorp.com/terraform/0.11.7/terraform_0.11.7_linux_amd64.zip
+unzip /tmp/packer.zip -d ~/bin
+unzip /tmp/terraform.zip -d ~/bin
+packer validate deployments/template.json &&
+packer build deployments/template.json &&
+export TF_VAR_image_id=$(curl -X GET -H"Content-Type: application/json" -H"Authorization: Bearer $DIGITALOCEAN_API_TOKEN""https://api.digitalocean.com/v2/images?private=true" | jq ."images[] | select(.name == \"platzi-demo-$CIRCLE_BUILD_NUM\") | .id") &&
+echo $TF_VAR_image_id
+cd infra && terraform apply && cd .. &&
+git add infra && git commit -m "Deployed $CIRCLE_BUILD_NUM [skip ci]" &&
+git push origin master
