772e42017b3e9a51aa9ea2413b030a246d76661c
Alex Guaman <alex.guaman86@gmail.com>
2018-07-24 22:29:40 -0500

    Full Deployment 4

--- deployments/prod.sh
+++ deployments/prod.sh
@@ -7,6 +7,6 @@ packer build deployments/template.json &&
 export TF_VAR_image_id=$(curl -H "Authorization: Bearer $DIGITALOCEAN_API_TOKEN" https://api.digitalocean.com/v2/images?private=true | jq ."images[] | select(.name == \"devops-demo-$CIRCLE_BUILD_NUM\") | .id") &&
 echo "Imagen:"
 echo $TF_VAR_image_id
-cd infra && **terraform init -input=false** && **terraform apply -input=false -auto-approve** && cd .. &&
+cd infra && terraform init -input=false && terraform apply -input=false -auto-approve && cd .. &&
 git add infra && git commit -m "Deployed $CIRCLE_BUILD_NUM [skip ci]" &&
 echo "Deployed finalized"
